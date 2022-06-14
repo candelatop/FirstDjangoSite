692ceb77720ab98009b4cf291efc476d617fa44e
Nikita Nikitin <candelax20@gmail.com>
2022-06-14 23:07:35 +0300

    Добавил страницы смены эмейла и пароля

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView,profile_login, BBLogoutView
+from .views import index, other_page, BBLoginView, profile_login, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView
 
 app_name = 'main'
 urlpatterns = [
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', index, name='index'),
     path('account/login', BBLoginView.as_view(), name='login'),
     path('account/profile', profile_login, name='profile'),
-    path('account/logout', BBLogoutView.as_view(), name='logout')
+    path('account/logout', BBLogoutView.as_view(), name='logout'),
+    path('account/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('account/password/change', BBPasswordChangeView.as_view(), name='password_change')
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+from .forms import ChangeUserInfoForm
+from .models import AdvUser
 
 
 def index(request):
@@ -30,3 +36,26 @@ class BBLogoutView(LoginRequiredMixin, LogoutView):
 @login_required
 def profile_login(request):
     return render(request, 'main/profile.html')
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя изменены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
+
+class BBPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'main/password_change.html'
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Пароль пользователя изменен'
